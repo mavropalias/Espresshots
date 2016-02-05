93743aecd619f846f401ff3f32cb27e934d7503d
Kostas Mavropalias <mavropalias@gmail.com>
2016-02-05 13:12:03 +0000

    Settings view (wip)

--- Espresshots/SettingsTVC.m
+++ Espresshots/SettingsTVC.m
@@ -31,72 +31,72 @@
 
 #pragma mark - Table view data source
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 1;
-}
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    switch (section) {
-        case 0:
-            return 1;
-            break;
-            
-        default:
-            return 0;
-            break;
-    }
-}
-
-- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
-    switch (section) {
-        case 0:
-            return @"Caffeine amount for one shot";
-            break;
-            
-        default:
-            return @"";
-            break;
-    }
-}
-
-- (NSString *)tableView:(UITableView *)tableView titleForFooterInSection:(NSInteger)section {
-    switch (section) {
-        case 0:
-            return @"The amount of caffeine in a shot can vary between coffees and coffee shops. The recommended default (75mg) provides a good average and makes it easy for you to focus on logging the number of shots in your coffee.";
-            break;
-            
-        default:
-            return @"";
-            break;
-    }
-}
-
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"shotAmount"];
-    cell.detailTextLabel.hidden = YES;
-    [[cell viewWithTag:3] removeFromSuperview];
-    
-    UITextField *textField = [[UITextField alloc] init];
-    textField.tag = 3;
-    textField.translatesAutoresizingMaskIntoConstraints = NO;
-    textField.keyboardType = UIKeyboardTypeNumberPad;
-    
-    [textField setEnabled:YES];
-    
-    [cell.contentView addSubview:textField];
-    
-    [cell addConstraint:[NSLayoutConstraint constraintWithItem:textField attribute:NSLayoutAttributeLeading relatedBy:NSLayoutRelationEqual toItem:cell.textLabel attribute:NSLayoutAttributeTrailing multiplier:1 constant:8]];
-    [cell addConstraint:[NSLayoutConstraint constraintWithItem:textField attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:cell.contentView attribute:NSLayoutAttributeTop multiplier:1 constant:8]];
-    [cell addConstraint:[NSLayoutConstraint constraintWithItem:textField attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:cell.contentView attribute:NSLayoutAttributeBottom multiplier:1 constant:-8]];
-    [cell addConstraint:[NSLayoutConstraint constraintWithItem:textField attribute:NSLayoutAttributeTrailing relatedBy:NSLayoutRelationEqual toItem:cell.detailTextLabel attribute:NSLayoutAttributeTrailing multiplier:1 constant:0]];
-    
-    textField.textAlignment = NSTextAlignmentRight;
-    textField.delegate = self;
-    textField.text = @"75mg";
-    
-    return cell;
-}
+//- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+//    return 1;
+//}
+//
+//- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+//    switch (section) {
+//        case 0:
+//            return 1;
+//            break;
+//            
+//        default:
+//            return 0;
+//            break;
+//    }
+//}
+//
+//- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+//    switch (section) {
+//        case 0:
+//            return @"Caffeine amount for one shot";
+//            break;
+//            
+//        default:
+//            return @"";
+//            break;
+//    }
+//}
+//
+//- (NSString *)tableView:(UITableView *)tableView titleForFooterInSection:(NSInteger)section {
+//    switch (section) {
+//        case 0:
+//            return @"The amount of caffeine in a shot can vary between coffees and coffee shops. The recommended default (75mg) provides a good average and makes it easy for you to focus on logging the number of shots in your coffee.";
+//            break;
+//            
+//        default:
+//            return @"";
+//            break;
+//    }
+//}
+//
+//
+//- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+//    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"shotAmount"];
+//    cell.detailTextLabel.hidden = YES;
+//    [[cell viewWithTag:3] removeFromSuperview];
+//    
+//    UITextField *textField = [[UITextField alloc] init];
+//    textField.tag = 3;
+//    textField.translatesAutoresizingMaskIntoConstraints = NO;
+//    textField.keyboardType = UIKeyboardTypeNumberPad;
+//    
+//    [textField setEnabled:YES];
+//    
+//    [cell.contentView addSubview:textField];
+//    
+//    [cell addConstraint:[NSLayoutConstraint constraintWithItem:textField attribute:NSLayoutAttributeLeading relatedBy:NSLayoutRelationEqual toItem:cell.textLabel attribute:NSLayoutAttributeTrailing multiplier:1 constant:8]];
+//    [cell addConstraint:[NSLayoutConstraint constraintWithItem:textField attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:cell.contentView attribute:NSLayoutAttributeTop multiplier:1 constant:8]];
+//    [cell addConstraint:[NSLayoutConstraint constraintWithItem:textField attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:cell.contentView attribute:NSLayoutAttributeBottom multiplier:1 constant:-8]];
+//    [cell addConstraint:[NSLayoutConstraint constraintWithItem:textField attribute:NSLayoutAttributeTrailing relatedBy:NSLayoutRelationEqual toItem:cell.detailTextLabel attribute:NSLayoutAttributeTrailing multiplier:1 constant:0]];
+//    
+//    textField.textAlignment = NSTextAlignmentRight;
+//    textField.delegate = self;
+//    textField.text = @"75mg";
+//    
+//    return cell;
+//}
 
 
 
